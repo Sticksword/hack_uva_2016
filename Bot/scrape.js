8b5447afe9762be05bf3daa15527c57fadc91efc
ericexiao <es2py@virginia.edu>
2016-03-26 21:58:28 -0400

    Facebook SDK for PHP

--- Bot/scrape.js
+++ Bot/scrape.js
@@ -1,5 +1,17 @@
-function scrape() {
-	$('._1qdd').each(function () {
-		console.log($(this).attr('href');
-	});
-}
+$(document).ready(function() {
+	$('._4cbb').each(function () {
+		var div_id = $(this).attr('id');
+		var event_id = div_id.substring(15);
+	  	console.log(div_id + " " + event_id);
+	  	$.ajax(function() {
+			url: "http://ericshiao.me/facebookScrape.php",
+			method: 'POST',
+			data: event_id;
+			success: function(response) {
+				console.log(response.name);
+			}
+		});
+	});	
+	
+});
+
