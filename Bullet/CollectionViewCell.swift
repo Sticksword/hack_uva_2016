7830276c2d94eb722ce77200efec8bb740d22f78
Jordan <jjl4mb@virginia.edu>
2016-03-27 03:51:26 -0400

    Random

--- Bullet/CollectionViewCell.swift
+++ Bullet/CollectionViewCell.swift
@@ -12,6 +12,27 @@ class CollectionViewCell: UICollectionViewCell {
     
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var heartPopup: UIImageView!
     
+    func showHeart(gesture: UIGestureRecognizer) {
+//        UIView.animateWithDuration(0.3, animations: {
+//                self.heartPopup.transform = CGAffineTransformMakeScale(1.3, 1.3)
+//                self.heartPopup.alpha = 1.0
+//            }, completion: {
+//                (value: Bool) in
+//                UIView.animateWithDuration(0.1, animations: {
+//                    self.heartPopup.transform = CGAffineTransformMakeScale(1.0, 1.0)
+//                    }, completion: {
+//                        (value: Bool) in
+//                        UIView.animateWithDuration(0.3, animations: {
+//                            self.heartPopup.transform = CGAffineTransformMakeScale(1.3, 1.3)
+//                            self.heartPopup.alpha = 0.0
+//                            }, completion: {
+//                                (value: Bool) in
+//                                self.heartPopup.transform = CGAffineTransformMakeScale(1.0, 1.0)
+//                        })
+//                })
+//            })
+    }
     
 }
